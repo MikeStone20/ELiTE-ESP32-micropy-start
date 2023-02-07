c50f41e1be4ea83056f328707aa6f23ab4ec4e33
Michael Stone <31086575+MikeStone20@users.noreply.github.com>
2023-02-07 18:40:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from helper import helper
 from time import sleep_ms
+from machine import I2C
 import ujson, urequests
+'''
+I2C object that allows controls communication between micro controller and sensors.
+27, 26 are pins on your esp32 board. Feel free to change to another pin.
+freq depends on the sensor you are deling with. 40K is a safe ballpark for most sensors.
+'''
+#I2C object to communicate with sensors
+i2c = I2C(1, 27, 26, freq=40000)
 
 # Object to use helper methods
 helper_obj = helper()
