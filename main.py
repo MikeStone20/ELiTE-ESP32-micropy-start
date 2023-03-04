0f82a07d325635606ef96464933020649eb08123
Michael Stone <31086575+MikeStone20@users.noreply.github.com>
2023-03-04 09:19:50 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from time import sleep_ms
 from machine import I2C, Pin
 import ujson, urequests
 from ahtx0 import AHT10
+
 '''
 I2C object that allows controls communication between micro controller and sensors.
 27, 26 are pins on your esp32 board. Feel free to change to another pin.
@@ -10,12 +11,15 @@ freq depends on the sensor you are deling with. 40K is a safe ballpark for most 
 '''
 #I2C object to communicate with sensors
 i2c = I2C(0, scl=Pin(27), sda=Pin(26), freq=40000)
-temp_sensor = AHT10(i2c)
+
 # Object to use helper methods
 helper_obj = helper()
 
+# Build an AHT10 object
+temp_sensor = AHT10(i2c)
+
 # Connects to wifi and synchronizes device time
-sta_if, rtc = helper_obj.self_calibrate('wifi-name', 'wifi-password', 'New_York')
+sta_if, rtc = helper_obj.self_calibrate('iStone', '', 'New_York')
 
 # Checking if wifi connection succeeded
 if sta_if:
@@ -30,6 +34,34 @@ if rtc:
 
 # data collection + data storage should go in while loop
 while True:
-    print('Hello world')
-    sleep_ms(1000)
     print(temp_sensor.temperature())
+    current_time = helper_obj.get_time_stamp()
+    temp = temp_sensor.temperature()
+    humidity = temp_sensor.relative_humidity()
+    temp_package = ujson.dumps({'timestamp': current_time, 'temp': temp})
+    humidity_package = ujson.dumps({'timestamp': current_time, 'humidity': humidity})
+    
+    if not sta_if.isconnected():
+        print('Wifi got disconnected. Attempting to reconnect.')
+        helper_obj.wifi_connect('iStone', '')
+    
+    res_humidity = None
+    try:
+        res_humidity = urequests.post('https://codenextchat-1ff54.firebaseio.com/sensor_data/aht10/temperature.json', data=temp_package)
+    except:
+        print('Unable to connect to database.')
+            
+    finally:
+        if res_humidity:
+            res_humidity.close()
+    
+    res_temp = None
+    try:
+        res_temp = urequests.post('https://codenextchat-1ff54.firebaseio.com/sensor_data/aht10/humidity.json', data=humidity_package)
+    except:
+            print('Unable to connect to database.')
+    finally:
+        if res_temp:
+            res_temp.close()
+    sleep_ms(10000)
+    print(current_time)
